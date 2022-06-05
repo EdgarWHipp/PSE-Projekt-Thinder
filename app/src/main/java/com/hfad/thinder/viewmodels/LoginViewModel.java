c1d62b94f1a97351e4e4270e9f639ab555fbd85d
Georg Melchior <GeMel@gmx.de>
2022-06-05 16:19:42 +0200

    LoginFormState entfernt

--- app/src/main/java/com/hfad/thinder/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/hfad/thinder/viewmodels/LoginViewModel.java
@@ -7,11 +7,9 @@ import java.util.regex.Pattern;
 
 //Todo: Javadoc schreiben.
 public class LoginViewModel extends ViewModel {
-    private MutableLiveData<LoginFormState> loginFormState = new MutableLiveData<>();
     private MutableLiveData<LoginResult> loginResult = new MutableLiveData<>();
     private Repository loginRepository = new Repository(); //Todo: Richtiges Repository fehlt
 
-    private static final Pattern PASSWORD_PATTERN = Pattern.compile();//Todo: geeignetes Pattern überlegen
 
 
     public MutableLiveData<LoginFormState> getLoginFormState() {
@@ -27,41 +25,6 @@ public class LoginViewModel extends ViewModel {
         Result restult = loginRepository.login(email, password);
         //Todo: Implementiere Festlegung des loginResults, wenn Implementierung des Repositories bekannt ist
     }
-
-    //Ändert den Zustand der Validität der Email und des Passworts
-    public void loginDataChanged(String email, String password) {
-        if (passwordFormIsValid(password)) {
-            loginFormState.setValue(new LoginFormState(true));
-        }
-    }
-
-    private boolean passwordFormIsValid(String password) {
-        if (password == null){
-            loginFormState.setValue(new LoginFormState("Passwort ist Null",false));
-            return false;
-        }
-        if(password.length() < 8) {
-            loginFormState.setValue(new LoginFormState("Passwort is zu kurz", false));
-            return false;
-        }
-        if(!PASSWORD_PATTERN.matcher(password).matches()) {
-            loginFormState.setValue(new LoginFormState("Passwort muss mindestens eine Zahl und einen Großbuchstaben enthalten", false));
-        return false;
-        }
-        return true;
-    }
-
-    //Todo: Eventuell Prüfung des E-Mail Formats
-
-
-
-
-
-
-
-
-
-
 
 
 
