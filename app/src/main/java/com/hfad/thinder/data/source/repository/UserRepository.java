6c66892dd60a42211c32157e09e05aeea3e18663
Edgar Hipp <edgar.hipp@online.de>
2022-06-06 17:15:39 +0200

    Erweiterung des UserRepository

--- app/src/main/java/com/hfad/thinder/data/source/repository/UserRepository.java
+++ app/src/main/java/com/hfad/thinder/data/source/repository/UserRepository.java
@@ -9,50 +9,80 @@ import java.util.Optional;
  * Singleton instance of a StudentRepository.
  * User data is saved locally in a List for the first local test run of the frontend.
  */
-public final class UserRepository implements BaseRepository {
-    private static UserRepository INSTANCE;
-    private List<User> users;
+public final class UserRepository implements BaseRepository<User> {
+  private static UserRepository INSTANCE;
+  private int userId = -1;
+  private List<User> users;
 
-    private UserRepository() {
+  private UserRepository() {
 
+  }
+
+  /**
+   * Get an instance of the user repository.
+   *
+   * @return
+   */
+  public static UserRepository getInstance() {
+    if (INSTANCE == null) {
+      INSTANCE = new UserRepository();
     }
 
-    /**
-     * Get an instance of the user repository.
-     *
-     * @return
-     */
-    public static UserRepository getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new UserRepository();
-        }
+    return INSTANCE;
+  }
 
-        return INSTANCE;
-    }
+  /**
+   * only used for local testing - ID information is handled in the backend.
+   * returns an incrementing userId for each user object to use.
+   *
+   * @return
+   */
+  public int iterUserId() {
+    return getInstance().userId++;
+  }
 
-    @Override
-    public List getAll() {
-        return users;
-    }
+  @Override
+  public List getAll() {
+    return users;
+  }
 
-    @Override
-    public Optional getById(int id) {
-        return Optional.empty();
-    }
+  @Override
+  public Optional getById(int id) {
+    return Optional.empty();
+  }
 
-    @Override
-    public boolean save(Object obj) {
-        return users.add((User) obj);
-    }
+  /**
+   * Adds the given user to the private local users list.
+   *
+   * @param user
+   * @return
+   */
+  @Override
+  public boolean save(User user) {
 
-    /**
-     * Deletes the user with the given id, if the call is not successful false is returned.
-     *
-     * @param id userId in User class
-     * @return
-     */
-    @Override
-    public boolean delete(int id) {
-        return users.removeIf(user -> user.getId() == id);
-    }
+
+    return users.add(user);
+  }
+
+  /**
+   * Deletes the user with the given id, if the call is not successful false is returned.
+   *
+   * @param id userId in User class
+   * @return
+   */
+  @Override
+  public boolean delete(int id) {
+    return users.removeIf(user -> user.getId() == id);
+  }
+
+  /**
+   * Checks if a user exists with the given password and email pair.
+   *
+   * @param password
+   * @param email
+   * @return
+   */
+  public boolean check(String password, String email) {
+    return users.stream().anyMatch(user -> user.getPassword().equals(password) && user.geteMail().equals(email));
+  }
 }
