3572ed2f38ec90b584ce6c36658853d32942d641
erfoip <upkou@student.kit.edu>
2022-06-10 16:04:54 +0200

    Buttons Änderung von .clickable zu .enabled, um es für den Nutzer eindeutiger zu machen.

--- app/src/main/java/com/hfad/thinder/data/model/User.java
+++ app/src/main/java/com/hfad/thinder/data/model/User.java
@@ -5,39 +5,39 @@ package com.hfad.thinder.data.model;
  */
 
 public class User {
-  private final String password;
+    private final String password;
 
-  private final String eMail;
+    private final String eMail;
 
-  private int userId;
+    private int userId;
 
-  private String firstName;
-  private String lastName;
+    private String firstName;
+    private String lastName;
 
-  public User(String password, String eMail,String firstName, String lastName) {
-    this.password = password;
-    this.eMail = eMail;
-    this.firstName=firstName;
-    this.lastName=lastName;
-  }
+    public User(String password, String eMail, String firstName, String lastName) {
+        this.password = password;
+        this.eMail = eMail;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public String geteMail() {
-    return eMail;
-  }
+    public String geteMail() {
+        return eMail;
+    }
 
-  public int getId() {
-    return userId;
-  }
+    public int getId() {
+        return userId;
+    }
 
-  public int getFirstName() {
-    return firstName;
-  }
+    public String getFirstName() {
+        return firstName;
+    }
 
-  public int getLastName() {
-    return lastName;
-  }
+    public String getLastName() {
+        return lastName;
+    }
 }
