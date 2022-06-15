ce678c59f17aae02a264a2fccc2c265861ce0146
Edgar Hipp <edgar.hipp@online.de>
2022-06-15 19:43:58 +0200

    Addition to generated HTTP PUT,POST,GET,DELETE requests in the model

---
+++ app/src/main/java/com/hfad/thinder/data/source/repository/Iterator.java
@@ -0,0 +1,21 @@
+package com.hfad.thinder.data.source.repository;
+
+public class Iterator {
+  private static Iterator INSTANCE;
+  private int counter;
+
+  private Iterator() {
+
+  }
+
+  public static Iterator getInstance() {
+    if (INSTANCE == null) {
+      INSTANCE = new Iterator();
+    }
+    return INSTANCE;
+  }
+
+  public int getIdCounter() {
+    return counter;
+  }
+}
