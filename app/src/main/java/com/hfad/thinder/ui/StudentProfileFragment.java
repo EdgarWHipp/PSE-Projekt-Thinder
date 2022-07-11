72d82abc9faac6433b8c20a672302b7444f98bf4
erfoip <upkou@student.kit.edu>
2022-07-11 18:13:17 +0200

    Studentprofil Layout

--- app/src/main/java/com/hfad/thinder/ui/StudentProfileFragment.java
+++ app/src/main/java/com/hfad/thinder/ui/StudentProfileFragment.java
@@ -2,25 +2,31 @@ package com.hfad.thinder.ui;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.hfad.thinder.R;
+import com.hfad.thinder.databinding.FragmentStudentProfileBinding;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link StudentProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class StudentProfileFragment extends Fragment {
+public class StudentProfileFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private FragmentStudentProfileBinding binding;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,6 +67,17 @@ public class StudentProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_profile, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_student_profile, container, false);
+
+        View view = binding.getRoot();
+        Button coursesOfStudyButton = binding.btAddCoursesOfStudy;
+        coursesOfStudyButton.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view){
+        Navigation.findNavController(view).navigate(R.id.action_studentProfileFragment_to_coursesOfStudyFragment2);
     }
 }
