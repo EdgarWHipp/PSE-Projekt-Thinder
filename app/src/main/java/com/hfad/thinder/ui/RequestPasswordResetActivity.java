df7e07f38ce76ff8bfd01a057381472f05fed378
erfoip <upkou@student.kit.edu>
2022-07-26 16:48:45 +0200

    RequestPasswordResetActivity

---
+++ app/src/main/java/com/hfad/thinder/ui/RequestPasswordResetActivity.java
@@ -0,0 +1,38 @@
+package com.hfad.thinder.ui;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import com.hfad.thinder.R;
+import com.hfad.thinder.databinding.ActivityRequestPasswordResetBinding;
+import com.hfad.thinder.viewmodels.user.ForgotPasswordViewModel;
+
+public class RequestPasswordResetActivity extends AppCompatActivity {
+
+    private ActivityRequestPasswordResetBinding binding;
+    private PasswordResetRequestViewModel viewmodel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_request_password_reset);
+        viewmodel = new ViewModelProvider(this).get(PasswordResetRequestViewModel.class);
+
+        // my_child_toolbar is defined in the layout file
+        Toolbar myChildToolbar = binding.toolbar;
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+}
