65224709b4e7194760fa27460526f9f23d1e5ac0
Georg Melchior <GeMel@gmx.de>
2022-07-27 13:25:18 +0200

    Request Password View Kommunikation mit View Model hergestellt.

--- app/src/main/java/com/hfad/thinder/ui/RequestPasswordResetActivity.java
+++ app/src/main/java/com/hfad/thinder/ui/RequestPasswordResetActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.hfad.thinder.R;
 import com.hfad.thinder.databinding.ActivityRequestPasswordResetBinding;
+import com.hfad.thinder.viewmodels.ViewModelResult;
 import com.hfad.thinder.viewmodels.user.PasswordResetRequestViewModel;
 
 public class RequestPasswordResetActivity extends AppCompatActivity {
@@ -35,9 +38,22 @@ public class RequestPasswordResetActivity extends AppCompatActivity {
 
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+
+        final Observer<ViewModelResult> resultObserver = new Observer<ViewModelResult>() {
+            @Override
+            public void onChanged(ViewModelResult viewModelResult) {
+                if (viewModelResult.isSuccess()) {
+                    goToForgotPasswordActivity();
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), viewModelResult.getErrorMessage(), Toast.LENGTH_LONG);
+                }
+            }
+        };
+
+        viewmodel.getResetRequestResult().observe(this, resultObserver);
     }
 
-    public void goToForgotPasswordActivity(View view){
+    public void goToForgotPasswordActivity(){
         Intent intent = new Intent(this, ForgotPasswordActivity.class);
         startActivity(intent);
     }
