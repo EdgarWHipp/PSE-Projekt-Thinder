f96b805f0ff916dc8a522a18139a005c47ae7c13
Georg Melchior <GeMel@gmx.de>
2022-08-09 16:46:56 +0200

    Fehler in ApiService Klassen behoben

--- app/src/main/java/com/hfad/thinder/data/source/remote/okhttp/DegreeApiService.java
+++ app/src/main/java/com/hfad/thinder/data/source/remote/okhttp/DegreeApiService.java
@@ -1,21 +1,15 @@
 package com.hfad.thinder.data.source.remote.okhttp;
 
 import androidx.annotation.NonNull;
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.hfad.thinder.data.model.Degree;
-import com.hfad.thinder.data.model.Thesis;
-import com.hfad.thinder.data.model.User;
 import com.hfad.thinder.data.source.repository.UserRepository;
 import com.hfad.thinder.data.source.result.Pair;
 import com.hfad.thinder.data.source.result.Result;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
@@ -29,61 +23,65 @@ import okhttp3.Response;
  */
 public class DegreeApiService {
   private static final MediaType JSON
-          = MediaType.parse("application/json; charset=utf-8");
+      = MediaType.parse("application/json; charset=utf-8");
   private static final OkHttpClient client = new OkHttpClient();
   private static final String url = "http://localhost:8080";
-  private static final String  emulatorLocalHost = "http://10.0.2.2:8080";
-  private String host="http";
-  private String scheme ="10.0.2.2";
-  private int port =8080;
+  private static final String emulatorLocalHost = "http://10.0.2.2:8080";
+  private final String host = "10.0.2.2";
+  private final String scheme = "http";
+  private final int port = 8080;
 
   /**
    * Implements the actual HTTP GET request that fetches all courses of study for the users university.
-   * @return Pair<CompletableFuture<ArrayList<Degree>>,CompletableFuture<Result>>
+   *
+   * @return Pair<CompletableFuture < ArrayList < Degree>>,CompletableFuture<Result>>
    */
-    public Pair<CompletableFuture<ArrayList<Degree>>,CompletableFuture<Result>> fetchAllCoursesOfStudyFuture(){
-      CompletableFuture<ArrayList<Degree>> degrees= new CompletableFuture<>();
-      CompletableFuture<Result> resultCompletableFuture = new CompletableFuture<>();
-      OkHttpClient clientAuth = new OkHttpClient.Builder()
-              .addInterceptor(
-                      new AuthInterceptor(UserRepository.getInstance().
-                              getUser().getMail(), UserRepository.getInstance().getPassword()))
-              .build();
-      HttpUrl url = new HttpUrl.Builder()
-              .scheme(scheme)
-              .host(host)
-              .port(port)
-              .addPathSegment("university")
-              .addPathSegment(UserRepository.getInstance().getUser().getUniversityId().toString())
-              .addPathSegment("degrees")
-              .build();
-      Request request = new Request.Builder()
-              .url(url)
-              .get()
-              .build();
-      Call call =  clientAuth.newCall(request);
-      call.enqueue(new Callback() {
-        @Override
-        public void onFailure(@NonNull Call call, @NonNull IOException e) {
+  public Pair<CompletableFuture<ArrayList<Degree>>, CompletableFuture<Result>> fetchAllCoursesOfStudyFuture() {
+    CompletableFuture<ArrayList<Degree>> degrees = new CompletableFuture<>();
+    CompletableFuture<Result> resultCompletableFuture = new CompletableFuture<>();
+    OkHttpClient clientAuth = new OkHttpClient.Builder()
+        .addInterceptor(
+            new AuthInterceptor(UserRepository.getInstance().
+                getUser().getMail(), UserRepository.getInstance().getPassword()))
+        .build();
+    HttpUrl url = new HttpUrl.Builder()
+        .scheme(scheme)
+        .host(host)
+        .port(port)
+        .addPathSegment("university")
+        .addPathSegment(UserRepository.getInstance().getUser().getUniversityId().toString())
+        .addPathSegment("degrees")
+        .build();
+    Request request = new Request.Builder()
+        .url(url)
+        .get()
+        .build();
+    Call call = clientAuth.newCall(request);
+    call.enqueue(new Callback() {
+      @Override
+      public void onFailure(@NonNull Call call, @NonNull IOException e) {
 
+      }
+
+      @Override
+      public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+        if (response.isSuccessful()) {
+          resultCompletableFuture.complete(new Result(true));
+          String body = response.body().string();
+          Gson gson = new Gson();
+
+          ArrayList<Degree> thesisList = (ArrayList<Degree>) gson.fromJson(response.body().string(),
+              new TypeToken<ArrayList<Degree>>() {
+              }.getType());
+          degrees.complete(thesisList);
+        } else {
+          resultCompletableFuture.complete(new Result("not successful", false));
         }
 
-        @Override
-        public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-          if(response.isSuccessful()){
-            resultCompletableFuture.complete(new Result(true));
-            String body = response.body().string();
-            Gson gson = new Gson();
-
-            ArrayList<Degree> thesisList = (ArrayList<Degree>)gson.fromJson(response.body().string(), new TypeToken<ArrayList<Degree>>(){}.getType());
-            degrees.complete(thesisList);
-          }else{
-            resultCompletableFuture.complete(new Result("not successful",false));
-          }
-
-        }
-      });
-      return new Pair<CompletableFuture<ArrayList<Degree>>,CompletableFuture<Result>>(degrees,resultCompletableFuture);
+      }
+    });
+    return new Pair<CompletableFuture<ArrayList<Degree>>, CompletableFuture<Result>>(degrees,
+        resultCompletableFuture);
   }
 
 
--- app/src/main/java/com/hfad/thinder/data/source/remote/okhttp/SupervisorApiService.java
+++ app/src/main/java/com/hfad/thinder/data/source/remote/okhttp/SupervisorApiService.java
@@ -1,22 +1,14 @@
 package com.hfad.thinder.data.source.remote.okhttp;
 
 import android.util.Log;
-
 import androidx.annotation.NonNull;
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.hfad.thinder.data.model.Login;
-import com.hfad.thinder.data.model.Student;
 import com.hfad.thinder.data.model.Supervisor;
 import com.hfad.thinder.data.model.Thesis;
 import com.hfad.thinder.data.source.repository.UserRepository;
-import com.hfad.thinder.data.source.result.Result;
 import com.hfad.thinder.data.source.result.Pair;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import com.hfad.thinder.data.source.result.Result;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,7 +16,6 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
@@ -33,13 +24,15 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SupervisorApiService {
   private static final MediaType JSON
-          = MediaType.parse("application/json; charset=utf-8");
-  private String host="http";
-  private String scheme ="10.0.2.2";
-  private int port =8080;
+      = MediaType.parse("application/json; charset=utf-8");
+  private String host = "10.0.2.2";
+  private String scheme = "http";
+  private int port = 8080;
 
   public String getHost() {
     return host;
@@ -67,7 +60,8 @@ public class SupervisorApiService {
 
   /**
    * This function creates the HTTP PUT request that completes the user profile by extending the profile through either the additional attributes from the supervisor.
-   *Checks if the asynchronous call return fails or responds.
+   * Checks if the asynchronous call return fails or responds.
+   *
    * @param degree
    * @param institute
    * @param phoneNumber
@@ -75,53 +69,56 @@ public class SupervisorApiService {
    * @throws JSONException
    * @throws IOException
    */
-  public CompletableFuture<Result> editSupervisorProfileFuture(String degree, String officeNumber,String building, String institute, String phoneNumber, String firstName, String lastName)
-          throws JSONException, IOException {
+  public CompletableFuture<Result> editSupervisorProfileFuture(String degree, String officeNumber,
+                                                               String building, String institute,
+                                                               String phoneNumber, String firstName,
+                                                               String lastName)
+      throws JSONException, IOException {
     CompletableFuture<Result> resultCompletableFuture = new CompletableFuture<>();
     OkHttpClient clientAuth = new OkHttpClient.Builder()
-            .addInterceptor(
-                    new AuthInterceptor(UserRepository.getInstance().
-                            getUser().getMail(), UserRepository.getInstance().
-                            getPassword()))
-            .build();
-    Log.e("",UserRepository.getInstance().
-            getUser().getMail()+UserRepository.getInstance().
-            getPassword());
+        .addInterceptor(
+            new AuthInterceptor(UserRepository.getInstance().
+                getUser().getMail(), UserRepository.getInstance().
+                getPassword()))
+        .build();
+    Log.e("", UserRepository.getInstance().
+        getUser().getMail() + UserRepository.getInstance().
+        getPassword());
     JSONObject supervisorJson = new JSONObject()
-            .put("academicDegree", degree)
-            .put("officeNumber", officeNumber)
-            .put("building",building)
-            .put("institute", institute)
-            .put("phoneNumber", phoneNumber)
-            .put("firstName",firstName)
-            .put("lastName",lastName)
-            .put("type","SUPERVISOR");
+        .put("academicDegree", degree)
+        .put("officeNumber", officeNumber)
+        .put("building", building)
+        .put("institute", institute)
+        .put("phoneNumber", phoneNumber)
+        .put("firstName", firstName)
+        .put("lastName", lastName)
+        .put("type", "SUPERVISOR");
 
 
     RequestBody body = RequestBody.create(supervisorJson.toString(), JSON);
-    Log.e("",supervisorJson.toString());
-  Log.e("",scheme);
+    Log.e("", supervisorJson.toString());
+    Log.e("", scheme);
     HttpUrl url = new HttpUrl.Builder()
-            .scheme(scheme)
-            .host(host)
-            .port(port)
-            .addPathSegment("users")
-            .addPathSegment("current")
-            .build();
+        .scheme(scheme)
+        .host(host)
+        .port(port)
+        .addPathSegment("users")
+        .addPathSegment("current")
+        .build();
     Request request = new Request.Builder()
-            .url(url)
-            .put(body)
-            .build();
+        .url(url)
+        .put(body)
+        .build();
     Call callSupervisor = clientAuth.newCall(request);
     callSupervisor.enqueue(new Callback() {
       @Override
       public void onFailure(@NonNull Call call, @NonNull IOException e) {
-        resultCompletableFuture.complete(new Result(e.toString(),false));
+        resultCompletableFuture.complete(new Result(e.toString(), false));
       }
 
       @Override
       public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-        if (response.isSuccessful()){
+        if (response.isSuccessful()) {
           Supervisor current = (Supervisor) UserRepository.getInstance().getUser();
           current.setFirstName(firstName);
           current.setLastName(lastName);
@@ -132,64 +129,67 @@ public class SupervisorApiService {
           current.setPhoneNumber(phoneNumber);
           UserRepository.getInstance().setUser(current);
           UserRepository.getInstance().setProfileComplete(true);
-          resultCompletableFuture.complete(new Result("success",true));
-        }else {
-          resultCompletableFuture.complete(new Result("error",false));
+          resultCompletableFuture.complete(new Result("success", true));
+        } else {
+          resultCompletableFuture.complete(new Result("error", false));
         }
       }
     });
 
     return resultCompletableFuture;
   }
+
   /**
    * The function that creates the actual HTTP PUT request that edits a specific thesis in the backend.
+   *
    * @param thesisId
    * @param thesis
    * @return A CompletableFuture<Result> that is later evaluated in the RemoteDataSource classes
    * @throws JSONException
    */
-  public CompletableFuture<Result> editThesisFuture(final UUID thesisId, Thesis thesis) throws JSONException {
+  public CompletableFuture<Result> editThesisFuture(final UUID thesisId, Thesis thesis)
+      throws JSONException {
     OkHttpClient clientAuth = new OkHttpClient.Builder()
-            .addInterceptor(new AuthInterceptor
-                    (UserRepository.getInstance().getUser().getMail(),
-                            UserRepository.getInstance().getUser().getPassword()))
-            .build();
+        .addInterceptor(new AuthInterceptor
+            (UserRepository.getInstance().getUser().getMail(),
+                UserRepository.getInstance().getUser().getPassword()))
+        .build();
     CompletableFuture<Result> resultCompletableFuture = new CompletableFuture<>();
     JSONObject thesisJSON = new JSONObject()
-            .put("name", thesis.getName())
-            .put("motivation", thesis.getMotivation())
-            .put("task", thesis.getTask())
-            .put("questionForm", thesis.getForm())
-            .put("images", thesis.getImages())
-            .put("possibleDegrees", thesis.getPossibleDegrees())
-            .put("supervisor", UserRepository.getInstance().getUser())
-            .put("supervisingProfessor", thesis.getSupervisingProfessor());
+        .put("name", thesis.getName())
+        .put("motivation", thesis.getMotivation())
+        .put("task", thesis.getTask())
+        .put("questionForm", thesis.getForm())
+        .put("images", thesis.getImages())
+        .put("possibleDegrees", thesis.getPossibleDegrees())
+        .put("supervisor", UserRepository.getInstance().getUser())
+        .put("supervisingProfessor", thesis.getSupervisingProfessor());
 
     HttpUrl url = new HttpUrl.Builder()
-            .scheme(scheme)
-            .host(host)
-            .port(port)
-            .addPathSegment("thesis")
-            .addPathSegment(thesisId.toString()).build();
+        .scheme(scheme)
+        .host(host)
+        .port(port)
+        .addPathSegment("thesis")
+        .addPathSegment(thesisId.toString()).build();
 
     RequestBody body = RequestBody.create(thesisJSON.toString(), JSON);
     Request request = new Request.Builder()
-            .url(url)
-            .put(body)
-            .build();
+        .url(url)
+        .put(body)
+        .build();
     Call call = clientAuth.newCall(request);
     call.enqueue(new Callback() {
       @Override
       public void onFailure(@NonNull Call call, @NonNull IOException e) {
-        resultCompletableFuture.complete(new Result("error",false));
+        resultCompletableFuture.complete(new Result("error", false));
       }
 
       @Override
       public void onResponse(@NonNull Call call, @NonNull Response response) {
-        if (response.isSuccessful()){
+        if (response.isSuccessful()) {
           resultCompletableFuture.complete(new Result(true));
-        }else {
-          resultCompletableFuture.complete(new Result("not successful",false));
+        } else {
+          resultCompletableFuture.complete(new Result("not successful", false));
         }
 
       }
@@ -198,52 +198,55 @@ public class SupervisorApiService {
   }
 
   /**
-   *  Performs the actual HTTP GET request that gets all already created theses from a supervisor.
-   * @return Pair<CompletableFuture<ArrayList<Thesis>>,CompletableFuture<Result>>
+   * Performs the actual HTTP GET request that gets all already created theses from a supervisor.
+   *
+   * @return Pair<CompletableFuture < ArrayList < Thesis>>,CompletableFuture<Result>>
    */
-  public Pair<CompletableFuture<HashMap<UUID,Thesis>>,CompletableFuture<Result>> getCreatedThesisFromSupervisorFuture(){
+  public Pair<CompletableFuture<HashMap<UUID, Thesis>>, CompletableFuture<Result>> getCreatedThesisFromSupervisorFuture() {
     OkHttpClient clientAuth = new OkHttpClient.Builder()
-            .addInterceptor(new AuthInterceptor
-                    (UserRepository.getInstance().getUser().getMail(),
-                            UserRepository.getInstance().getPassword()))
-            .build();
+        .addInterceptor(new AuthInterceptor
+            (UserRepository.getInstance().getUser().getMail(),
+                UserRepository.getInstance().getPassword()))
+        .build();
     CompletableFuture<Result> resultCompletableFuture = new CompletableFuture<>();
-    CompletableFuture<HashMap<UUID,Thesis>> thesisHashmap = new CompletableFuture<>();
+    CompletableFuture<HashMap<UUID, Thesis>> thesisHashmap = new CompletableFuture<>();
 
     HttpUrl url = new HttpUrl.Builder()
-            .scheme(scheme)
-            .host(host)
-            .port(port)
-            .addPathSegment("theses").build();
+        .scheme(scheme)
+        .host(host)
+        .port(port)
+        .addPathSegment("theses").build();
 
     Request request = new Request.Builder()
-            .url(url)
-            .get()
-            .build();
+        .url(url)
+        .get()
+        .build();
     Call call = clientAuth.newCall(request);
     call.enqueue(new Callback() {
       @Override
       public void onFailure(@NonNull Call call, @NonNull IOException e) {
-        resultCompletableFuture.complete(new Result("not successful",false));
+        resultCompletableFuture.complete(new Result("not successful", false));
 
       }
 
       @Override
       public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-        if(response.isSuccessful()){
+        if (response.isSuccessful()) {
 
           resultCompletableFuture.complete(new Result(true));
-            Gson gson = new Gson();
-            String body = response.body().string();
-            ArrayList<Thesis> theses  = gson.fromJson(body, new TypeToken<List<Thesis>>(){}.getType());
-            HashMap<UUID,Thesis> thesisHashMap = (HashMap<UUID, Thesis>) theses.stream().collect(Collectors.toMap(v->v.getId(), v->v));
-            resultCompletableFuture.complete(new Result(true));
-            thesisHashmap.complete(thesisHashMap);
-        }else{
-          resultCompletableFuture.complete(new Result("not successful",false));
+          Gson gson = new Gson();
+          String body = response.body().string();
+          ArrayList<Thesis> theses = gson.fromJson(body, new TypeToken<List<Thesis>>() {
+          }.getType());
+          HashMap<UUID, Thesis> thesisHashMap = (HashMap<UUID, Thesis>) theses.stream()
+              .collect(Collectors.toMap(v -> v.getId(), v -> v));
+          resultCompletableFuture.complete(new Result(true));
+          thesisHashmap.complete(thesisHashMap);
+        } else {
+          resultCompletableFuture.complete(new Result("not successful", false));
         }
       }
     });
-    return new Pair<>(thesisHashmap,resultCompletableFuture);
+    return new Pair<>(thesisHashmap, resultCompletableFuture);
   }
 }
