cced4eb260164cc02e2bc2e8f3a0ce608b092349
Georg Melchior <GeMel@gmx.de>
2022-08-10 12:47:11 +0200

    Leere Liste an erstellten Arbeiten verursachen keinen Absturz mehr

--- app/src/main/java/com/hfad/thinder/viewmodels/student/LikedThesesViewModel.java
+++ app/src/main/java/com/hfad/thinder/viewmodels/student/LikedThesesViewModel.java
@@ -2,14 +2,11 @@ package com.hfad.thinder.viewmodels.student;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
 import com.hfad.thinder.data.model.Thesis;
 import com.hfad.thinder.data.source.repository.ThesisRepository;
 import com.hfad.thinder.viewmodels.ThesisCardItem;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
@@ -17,7 +14,6 @@ import java.util.UUID;
 public class LikedThesesViewModel extends ViewModel {
   private static final ThesisRepository thesisRepository = ThesisRepository.getInstance();
   private MutableLiveData<ArrayList<ThesisCardItem>> likedTheses;
-  private Bitmap image;//Todo löschen
 
 
   //--------------------getter and setter----------------------------------------------------
@@ -26,14 +22,7 @@ public class LikedThesesViewModel extends ViewModel {
   public MutableLiveData<ArrayList<ThesisCardItem>> getLikedTheses() {
     if (likedTheses == null) {
       likedTheses = new MutableLiveData<>();
-      //Todo löschen
-      image = Bitmap.createBitmap(100, 50, Bitmap.Config.ARGB_8888);
-      ThesisCardItem thesisCardItem = new ThesisCardItem(UUID.randomUUID(), "title", "task", image);
-      ArrayList<ThesisCardItem> items = new ArrayList<>();
-      items.add(thesisCardItem);
-      likedTheses.setValue(items);
-
-      //loadLikedTheses();
+      loadLikedTheses();
     }
     return likedTheses;
   }
@@ -44,8 +33,9 @@ public class LikedThesesViewModel extends ViewModel {
     HashMap<UUID, Thesis> likedTheses = thesisRepository.getThesisMap();
     for (Thesis thesis : likedTheses.values()) {
       byte[] byteArray = thesis.getImages().iterator().next().getImage();
-      Bitmap image = BitmapFactory.decodeByteArray(byteArray, 0 , byteArray.length);
-      ThesisCardItem thesisCardItem = new ThesisCardItem(thesis.getId(), thesis.getName(), thesis.getTask(), image);
+      Bitmap image = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+      ThesisCardItem thesisCardItem =
+          new ThesisCardItem(thesis.getId(), thesis.getName(), thesis.getTask(), image);
       thesisCards.add(thesisCardItem);
     }
     this.likedTheses.setValue(thesisCards);
