8d572aa90066a30ff14cc5ca8e26366f79ad8a9e
Georg Melchior <GeMel@gmx.de>
2022-08-18 21:12:17 +0200

    View Models weiterer JavaDoc

--- app/src/main/java/com/hfad/thinder/viewmodels/ViewModelResult.java
+++ app/src/main/java/com/hfad/thinder/viewmodels/ViewModelResult.java
@@ -1,35 +1,52 @@
 package com.hfad.thinder.viewmodels;
 
 import androidx.annotation.Nullable;
+import com.hfad.thinder.data.source.result.Result;
 
-//Enthält informationen darüber, ob der Login erfolgreich war
+/**
+ * A class representing the result of call to the model. Contains a {@link ViewModelResultTypes ViewModelResultType}
+ * and in case of the call returns an error a corresponding error message.
+ */
 public class ViewModelResult {
 
-    @Nullable
-    private final String errorMessage;
+  @Nullable
+  private final String errorMessage;
 
-    @Nullable
-    private final ViewModelResultTypes viewModelResultTypes;
+  @Nullable
+  private final ViewModelResultTypes viewModelResultType;
 
+  /**
+   * @param errorMessage        an error message
+   * @param viewModelResultType a {@link ViewModelResultTypes ViewModelResultType}
+   */
+  public ViewModelResult(@Nullable String errorMessage, @Nullable
+      ViewModelResultTypes viewModelResultType) {
+    this.errorMessage = errorMessage;
+    this.viewModelResultType = viewModelResultType;
+  }
 
-    public ViewModelResult(@Nullable String errorMessage, @Nullable
-            ViewModelResultTypes viewModelResultTypes) {
-        this.errorMessage = errorMessage;
-        this.viewModelResultTypes = viewModelResultTypes;
-    }
+  /**
+   * @return the error message produced by the model call. Returns null if no error occurred.
+   */
+  @Nullable
+  public String getErrorMessage() {
+    return this.errorMessage;
+  }
 
+  /**
+   * @return the {@link ViewModelResultTypes ViewModelResultType} of the the model call.
+   */
+  @Nullable
+  public ViewModelResultTypes getSuccess() {
+    return this.viewModelResultType;
+  }
 
-    @Nullable
-    public String getErrorMessage() {
-        return this.errorMessage;
-    }
-
-    @Nullable
-    public ViewModelResultTypes getSuccess() {
-        return this.viewModelResultTypes;
-    }
-
-    public boolean isSuccess() {
-        return this.viewModelResultTypes == ViewModelResultTypes.SUCCESSFUL;
-    }
+  /**
+   * @return true if {@link Result#getSuccess()} is true, false otherwise.
+   */
+  public boolean isSuccess() {
+    return this.viewModelResultType == ViewModelResultTypes.SUCCESSFUL ||
+        this.viewModelResultType == ViewModelResultTypes.STUDENT ||
+        this.viewModelResultType == ViewModelResultTypes.SUPERVISOR;
+  }
 }
