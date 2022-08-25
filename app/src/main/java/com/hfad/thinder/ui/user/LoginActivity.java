5b4c9215b385050b2c58c624f4a8123949e37613
erfoip <upkou@student.kit.edu>
2022-08-25 10:19:45 +0200

    Verstecken der Tastatur beim Klicken des Login-Buttons.

--- app/src/main/java/com/hfad/thinder/ui/user/LoginActivity.java
+++ app/src/main/java/com/hfad/thinder/ui/user/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.hfad.thinder.ui.user;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -33,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         viewmodel = new ViewModelProvider(this).get(LoginViewModel.class);
         // assign the component to a property in the binding class
         mBinding.setViewmodel(viewmodel);
+        mBinding.setActivity(this);
         mBinding.setLifecycleOwner(this);
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
@@ -102,6 +105,17 @@ public class LoginActivity extends AppCompatActivity {
     private void gotToVerifyTokenActivity() {
         Intent intent = new Intent(this, VerifyTokenActivity.class);
         startActivity(intent);
+    }
+
+    public void login(){
+        // Check if no view has focus:
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            //hide keyboard
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+        viewmodel.login();
     }
 
     @Override
