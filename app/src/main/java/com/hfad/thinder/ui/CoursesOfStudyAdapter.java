b430fdc60c9e882c16c0a41a0e56156cc9e0bcdd
erfoip <upkou@student.kit.edu>
2022-08-31 13:32:29 +0200

    Java-Doc CoursesOfStudyAdapter

--- app/src/main/java/com/hfad/thinder/ui/CoursesOfStudyAdapter.java
+++ app/src/main/java/com/hfad/thinder/ui/CoursesOfStudyAdapter.java
@@ -1,13 +1,9 @@
 package com.hfad.thinder.ui;
 
-import static android.content.ContentValues.TAG;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.Filter;
 import android.widget.Filterable;
 
@@ -20,12 +16,23 @@ import com.hfad.thinder.viewmodels.CoursesOfStudyPicker;
 
 import java.util.ArrayList;
 
+/**
+ *  Is responsible for displaying the given data by the ViewModel in a RecyclerView.
+ *  Also handles click events and filter operations.
+ */
 public class CoursesOfStudyAdapter extends RecyclerView.Adapter<CoursesOfStudyAdapter.CoursesOfStudyViewHolder> implements Filterable {
 
     private ArrayList<CourseOfStudyItem> elements;
     private ArrayList<CourseOfStudyItem> elementsFull;
     private CoursesOfStudyPicker viewModel;
+
     private Filter elementsFilter = new Filter() {
+        /**
+         * returns all objects corresponding to the given charSequence
+         *
+         * @param charSequence filter input
+         * @return             filtered output
+         */
         @Override
         protected FilterResults performFiltering(CharSequence charSequence) {
             ArrayList<CourseOfStudyItem> filteredList = new ArrayList<>();
@@ -47,6 +54,12 @@ public class CoursesOfStudyAdapter extends RecyclerView.Adapter<CoursesOfStudyAd
             return results;
         }
 
+        /**
+         * Once filtering is completed this method performs the actual data change
+         *
+         * @param charSequence  filter input
+         * @param filterResults results of filtering
+         */
         @Override
         protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
             elements.clear();
@@ -55,6 +68,11 @@ public class CoursesOfStudyAdapter extends RecyclerView.Adapter<CoursesOfStudyAd
         }
     };
 
+    /**
+     * Constructor
+     *
+     * @param viewModel allows access to the actual data
+     */
     public CoursesOfStudyAdapter(CoursesOfStudyPicker viewModel) {
         this.viewModel = viewModel;
         this.elements = new ArrayList<>();
@@ -62,7 +80,13 @@ public class CoursesOfStudyAdapter extends RecyclerView.Adapter<CoursesOfStudyAd
         elementsFull = new ArrayList<>(elements);
     }
 
-    @NonNull
+    /**
+     * Creates new viewholder
+     *
+     * @param parent    ParentViewGroup
+     * @param viewType  type of the view
+     * @return          CourseOfStudyViewHolder
+     */
     @Override
     public CoursesOfStudyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_courses_of_study, parent, false);
@@ -70,11 +94,16 @@ public class CoursesOfStudyAdapter extends RecyclerView.Adapter<CoursesOfStudyAd
         return coursesOfStudyViewHolder;
     }
 
+    /**
+     * Binds data to views
+     *
+     * @param holder    viewholder holding the views
+     * @param position  position used to get the data
+     */
     @Override
     public void onBindViewHolder(@NonNull CoursesOfStudyViewHolder holder, int position) {
         int currentPosition = position;
         CourseOfStudyItem current = elements.get(position);
-        Log.i(TAG, current.getCourseOfStudy() + " " + current.isPicked());
         holder.mCheckBox.setText(current.getCourseOfStudy());
         holder.mCheckBox.setChecked(current.isPicked());
 
@@ -87,26 +116,50 @@ public class CoursesOfStudyAdapter extends RecyclerView.Adapter<CoursesOfStudyAd
         });
     }
 
+    /**
+     * returns number of elements in recyclerview
+     *
+     * @return Integer
+     */
     @Override
     public int getItemCount() {
         return elements.size();
     }
 
+    /**
+     * returns filter
+     *
+     * @return Filter
+     */
     @Override
     public Filter getFilter() {
         return elementsFilter;
     }
 
+    /**
+     * Sets elements of recyclerview and notifies about changed data
+     *
+     * @param elements new elements
+     */
     public void setElements(ArrayList<CourseOfStudyItem> elements) {
         this.elements = elements;
         notifyDataSetChanged();
     }
 
+    /**
+     *  Holds all the views of one recyclerview item. Implements OnClickListener to recognize clicks
+     *  to the views
+     */
     public static class CoursesOfStudyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public CheckBox mCheckBox;
 
         ItemClickListener itemClickListener;
 
+        /**
+         * Constructor
+         *
+         * @param itemView view of viewholder
+         */
         public CoursesOfStudyViewHolder(@NonNull View itemView) {
             super(itemView);
 
@@ -115,16 +168,29 @@ public class CoursesOfStudyAdapter extends RecyclerView.Adapter<CoursesOfStudyAd
             mCheckBox.setOnClickListener(this);
         }
 
+        /**
+         * Used for observer pattern. Registers ItemClickListener
+         *
+         * @param ic itemclicklistener
+         */
         public void setItemClickListener(ItemClickListener ic){
             this.itemClickListener = ic;
         }
 
+        /**
+         * Allows recognition of click input
+         *
+         * @param view object that has been clicked
+         */
         @Override
         public void onClick(View view) {
             this.itemClickListener.onItemClick(view, getLayoutPosition());
         }
 
-        interface ItemClickListener {
+        /**
+         *  used for handling click events
+         */
+        private interface ItemClickListener {
             void onItemClick(View v, int pos);
         }
     }
